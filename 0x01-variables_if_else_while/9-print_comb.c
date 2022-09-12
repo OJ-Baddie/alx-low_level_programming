00847f90b31214f9cf8525df45501e44607cfda7
Jane Badmus <mail2ojbadmus@gmail.com>
2022-09-11 23:28:43 -0700

    task 9 C variable

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	if (x <= '9')
+
+         	putchar(',');
+	        putchar(' ');
+	}
+	return (0);
+}
