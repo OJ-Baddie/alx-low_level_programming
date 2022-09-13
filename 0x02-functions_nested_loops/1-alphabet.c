af5c8f470ab387405e97c15975a0913a4bac7e95
Jane Badmus <mail2ojbadmus@gmail.com>
2022-09-13 07:46:10 -0700

    task 1 C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,2 +1,1 @@
-int _putchar(int);
 void print_alphabet(void);
