1c440fea354f3af2b1033510f566b9997b97cfea
Jane Badmus <mail2ojbadmus@gmail.com>
2022-09-15 15:20:53 -0700

    task 3  C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int z = 48;
+
+	for (z = 48; z < 58; z++)
+	{
+		_putchar(z);
+		z++;
+	}
+	_putchar('\n');
+
+}
