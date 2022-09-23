034fa013ca6605d36117526ea867751f3ed6a2d1
Jane Badmus <mail2ojbadmus@gmail.com>
2022-09-23 14:34:15 -0700

    task 5 more pointers, array and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,30 +1,23 @@
 #include "main.h"
+
 /**
  * cap_string - capitalizes all words of a string
  * @s: input string.
+ * string_toupper
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int x;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (i = 0; i < 13; i++)
+		if (s[x] >= 'a' && s[x] <= 'z')
 		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
+			s[x] = s[x] - 32;
 		}
-		count++;
 	}
+
 	return (s);
 }
